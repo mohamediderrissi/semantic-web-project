0513188aed6853c807b866530e15e1b7b121407d
mohamediderrissi <mohamediderrissi@gmail.com>
2020-11-27 23:30:41 +0100

    add ReactMapGl component for maps

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import ReactMapGl from 'react-map-gl';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    viewport: {
+      latitude: 45.4211,
+      longitude: -75.6903,
+      width: "100vw",
+      height: "100vh",
+      zoom: 10
+    }
+  };
   render() {
     return (
       <div className="App">
@@ -11,8 +22,17 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js </code> and save to reload.
         </p>
+        <div width="500px" height="400px">
+          <ReactMapGl { ...this.state.viewport}
+          mapStyle="mapbox://styles/jeuxconflit/cki0u2ojv2n5o1aluozw00egq" 
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          onViewportChange={(viewport) => this.setState({viewport: viewport })}
+          >
+            
+          </ReactMapGl>
+        </div>
       </div>
     );
   }
